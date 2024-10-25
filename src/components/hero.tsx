dc2b922878458caf697368a9f311d2b5ac44348b
Gokul <gokulgk9402@gmail.com>
2024-10-25 11:09:06 +0530

    minor responsive changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ import { Send } from "lucide-react";
 
 const HeroSection = () => {
   return (
-    <div className=" h-[calc(100vh-5rem)] flex flex-col lg:flex-row items-center lg:pb-[30vh] justify-center gap-6">
+    <div className=" h-[calc(100vh-5rem)] flex flex-col md:flex-row md:items-center md:pb-[30vh] md:justify-center pb-8 gap-6">
       <Image
         src={"/image.jpg"}
         width={600}
@@ -14,13 +14,13 @@ const HeroSection = () => {
         className=" h-2/3  object-contain w-auto"
         priority
       />
-      <div className=" flex flex-col gap-2">
-        <p className=" text-4xl font-thin">Hi, I&apos;m</p>
-        <h1 className=" text-6xl font-bold">Gokul Kannan</h1>
-        <h2 className=" text-4xl ">Software Developer</h2>
-        <Button className=" w-fit gap-2 mt-4" size={"lg"}>
-          <span className=" text-lg">Contact</span>
-          <Send size={20} />
+      <div className=" flex flex-col gap-2 px-4 md:px-0">
+        <p className=" text-2xl md:text-4xl font-thin">Hi, I&apos;m</p>
+        <h1 className=" text-4xl md:text-6xl font-bold">Gokul Kannan</h1>
+        <h2 className=" text-2xl  md:text-4xl ">Software Developer</h2>
+        <Button className=" w-fit gap-2 mt-4" size="lg">
+          <span className=" text-sm md:text-lg">Contact</span>
+          <Send size={18} />
         </Button>
       </div>
     </div>
