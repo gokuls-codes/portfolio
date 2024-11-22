3b6d2e0cb86020ac71afe09711a0d3c68980d7ec
Gokul <gokulgk9402@gmail.com>
2024-11-22 21:42:46 +0530

    minor fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,4 @@
-import ExperienceSection from "@/components/experience";
-import Footer from "@/components/footer";
-import HeroSection from "@/components/hero";
 import PointerGlow from "@/components/pointer-glow";
-// import ProjectsSection from "@/components/projects";
-import SkillsSection from "@/components/skills";
 import TopBar from "@/components/top-bar";
 
 export default function About() {
