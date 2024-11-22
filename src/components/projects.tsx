762b69e3899d08e03484c0d7fdf9b942260cd03f
Gokul <gokulgk9402@gmail.com>
2024-11-22 10:30:06 +0530

    added card border animation for projects section

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,6 +7,12 @@ const ProjectsSection = () => {
       id="experience"
     >
       <h2 className="  text-4xl font-semibold text-center">Projects</h2>
+      <div className=" w-full h-[400px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-screen-lg mx-auto p-4">
+        <div className=" mx-auto project-card w-full aspect-[2/3] bg-card rounded-lg relative"></div>
+        <div className=" mx-auto project-card w-full aspect-[2/3] bg-card rounded-lg relative"></div>
+        <div className=" mx-auto project-card w-full aspect-[2/3] bg-card rounded-lg relative"></div>
+        <div className=" mx-auto project-card w-full aspect-[2/3] bg-card rounded-lg relative"></div>
+      </div>
     </section>
   );
 };
