72ad8629c69183f09e1414ea2025561774833b6d
Gokul <gokulgk9402@gmail.com>
2024-11-25 19:04:48 +0530

    added something in test page

--- src/app/test/opposite-scroll/page.tsx
+++ src/app/test/opposite-scroll/page.tsx
@@ -3,205 +3,209 @@ import React from "react";
 
 const OppScrollPage = () => {
   return (
-    <div className=" h-[200vh] bg-blue-900 border-red-200 border  ">
-      <div className=" flex z-0 sticky top-0">
-        <div className=" h-screen  bg-blue-500 w-[600px]  top-0 border-red-200 border ">
-          <div className=" test-opp-scroll">
-            <Image
-              src="/on-the-go-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-            <Image
-              src="/expenses-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+    <>
+      <div className=" h-screen w-screen bg-orange-500"></div>
+      <div className=" h-[200vh] bg-blue-900 border-red-200 border  ">
+        <div className=" flex z-0 sticky top-0">
+          <div className=" h-screen overflow-clip  bg-blue-500 w-[600px]  top-0 border-red-200 border ">
+            <div className=" test-opp-scroll">
+              <Image
+                src="/on-the-go-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+              <Image
+                src="/expenses-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/rdt-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/rdt-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/sudoku-solver.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/sudoku-solver.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/on-the-go-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-            <Image
-              src="/expenses-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/on-the-go-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+              <Image
+                src="/expenses-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/rdt-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/rdt-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/sudoku-solver.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/sudoku-solver.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/on-the-go-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-            <Image
-              src="/expenses-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/on-the-go-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+              <Image
+                src="/expenses-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/rdt-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/rdt-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
 
-            <Image
-              src="/sudoku-solver.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
+              <Image
+                src="/sudoku-solver.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+            </div>
+          </div>
+          <div className=" h-screen overflow-clip bg-blue-500 w-[600px] sticky top-0 border-red-200 border ">
+            <div className="test-opp-scroll-reverse translate-y-[100%]">
+              <Image
+                src="/on-the-go-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+              <Image
+                src="/expenses-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/rdt-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/sudoku-solver.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/on-the-go-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+              <Image
+                src="/expenses-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/rdt-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/sudoku-solver.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/on-the-go-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+              <Image
+                src="/expenses-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/rdt-preview.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+
+              <Image
+                src="/sudoku-solver.png"
+                height={400}
+                width={600}
+                alt="On the go"
+                className=" h-[25vh] w-auto"
+              />
+            </div>
           </div>
         </div>
-        {/* <div className=" h-screen bg-blue-500 w-[600px] sticky top-0 border-red-200 border ">
-          <div className="test-opp-scroll-reverse translate-y-[100%]">
-            <Image
-              src="/on-the-go-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-            <Image
-              src="/expenses-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/rdt-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/sudoku-solver.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/on-the-go-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-            <Image
-              src="/expenses-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/rdt-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/sudoku-solver.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/on-the-go-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-            <Image
-              src="/expenses-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/rdt-preview.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-
-            <Image
-              src="/sudoku-solver.png"
-              height={400}
-              width={600}
-              alt="On the go"
-              className=" h-[25vh] w-auto"
-            />
-          </div>
-        </div> */}
       </div>
-      <div className=" h-screen bg-background z-20">
+
+      <div className=" h-screen bg-violet-400 z-20">
         <h1 className=" text-4xl font-bold text-white z-20">Text here</h1>
       </div>
-    </div>
+    </>
   );
 };
 
